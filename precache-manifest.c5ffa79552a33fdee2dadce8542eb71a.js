5dc9d2d2d8130c912468a8117cdb874a8e75feaf
Wilson Tu <wilson@Wilsons-MacBook-Pro.local>
2020-06-27 21:42:42 -0700

    Updates

---
+++ precache-manifest.c5ffa79552a33fdee2dadce8542eb71a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367161111e04fd3c17c28029483f4c8e",
+    "url": "/covid19Tracker/index.html"
+  },
+  {
+    "revision": "c59ce7e58dd85e087252",
+    "url": "/covid19Tracker/static/css/main.f557d1c5.chunk.css"
+  },
+  {
+    "revision": "25dcc3ac80f3301ad3d8",
+    "url": "/covid19Tracker/static/js/2.dd61c2d9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19Tracker/static/js/2.dd61c2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59ce7e58dd85e087252",
+    "url": "/covid19Tracker/static/js/main.f045bf46.chunk.js"
+  },
+  {
+    "revision": "49ae54595ed40eb5d52f",
+    "url": "/covid19Tracker/static/js/runtime-main.0aa77fb0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19Tracker/static/media/image.d7265326.png"
+  }
+]);
